8c9d4b1514a28be60de6709e3070bf9a0630edac
israel <israelpadonu14@gmail.com>
2025-03-30 00:52:51 +0100

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import  { useState } from "react";
 import { Link } from "react-router";
 import { Menu, X, } from "lucide-react";
 import { ThemeToggle } from "./ui/ThemeToggle";
-import { Button } from "./ui/Button";
+
 
 export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
